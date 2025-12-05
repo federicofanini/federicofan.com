995d46a5b5a58f558f5b9e9858a8ac7fb7ad375c
federicofanini <federico.fanini9@gmail.com>
2025-12-05 10:47:08 +0100

    short links

---
+++ src/app/(redirects)/tips/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function TipsPage() {
+  return redirect("https://federicofan.com/feedback");
+}
---
+++ src/app/(redirects)/x/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function XPage() {
+  return redirect("https://x.com/FedericoFan");
+}
